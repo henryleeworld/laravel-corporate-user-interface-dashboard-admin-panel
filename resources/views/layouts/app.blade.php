258001be11dd6a421792987182a3fb0a751232da
Henry Lee <henryleeworld@gmail.com>
2024-07-29 00:13:34 +0800

    feat:更新最新資料

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,8 +93,10 @@
             <footer class="footer p-5">
                 <div class="row align-items-center justify-content-lg-between">
                     <div class="col-lg-6 mb-lg-0 mb-4">
-                        <div class="copyright text-center text-m text-muted text-lg-start">
-                            ©<script>document.write(new Date().getFullYear());</script> {{ __('by Henry Lee') }}
+                        <div id="copyright" class="copyright text-center text-m text-muted text-lg-start">
+                            ©<script>
+                                document.getElementById("copyright").appendChild(document.createTextNode(new Date().getFullYear()));
+                            </script> {{ __('by Henry Lee') }}
                         </div>
                     </div>
                     <div class="col-lg-6">
